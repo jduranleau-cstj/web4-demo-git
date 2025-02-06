88713d2a0e11974816716f6ca10abafb177f0cf7
Julien Duranleau <jduranleau@cstj.qc.ca>
2025-02-06 09:47:05 -0500

    Balles qui bougent

--- js/main.js
+++ js/main.js
@@ -5,18 +5,31 @@ const balles = []
 for (let balise of balises_balles) {
     const balle = {
         balise: balise,
-        x: 0,
-        y: 0,
+        x: random(100, window.innerWidth - 100),
+        y: random(100, window.innerHeight - 100),
+        vx: random(-1, 1),
+        vy: random(-1, 1),
         grosseur: 20,
     }
+
+    balle.balise.style.width = balle.grosseur + "px"
 
     balles.push(balle)
 }
 
 function update() {
     for (let balle of balles) {
-        // do the thing
+
+        balle.x += balle.vx
+        balle.y += balle.vy
+
+        balle.balise.style.left = balle.x + "px"
+        balle.balise.style.top = balle.y + "px"
     }
 }
 
 setInterval(update, 1000 / 60) // 60 fps
+
+function random(min, max) {
+    return min + (Math.random() * (max - min))
+}
